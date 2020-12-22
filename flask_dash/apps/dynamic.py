01591220c0ed41b8f21b0083665713f50028962b
James Troy <james.m.troy@gmail.com>
2020-12-22 22:42:43 +0000

    add extra pages

---
+++ flask_dash/apps/dynamic.py
@@ -0,0 +1,145 @@
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+from dash.dependencies import Input, Output
+from flask import url_for
+
+from flask_dash.app import app
+
+
+def layout():
+    df = pd.read_csv(
+        "https://opendata-geohive.hub.arcgis.com/datasets/d8eb52d56273413b84b0187a4e9117be_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D"
+    )
+
+    colors = {"background": "#AAA", "text": "#7FDBFF"}
+
+    return html.Div(
+        [
+            dcc.Location(id="url", refresh=False),
+            html.Div(
+                children=[
+                    # All elements from the top of the page
+                    html.Div(
+                        [
+                            html.H1(children="Hello Dash"),
+                            html.Div(
+                                children="""
+            Dash: A web application framework for Python.
+        """
+                            ),
+                            dcc.Graph(
+                                id="Total Confirmed Cases",
+                                figure={
+                                    "data": [
+                                        {
+                                            "x": df["Date"],
+                                            "y": df["TotalConfirmedCovidCases"],
+                                            "type": "bar",
+                                            "name": "Total Confirmed Cases",
+                                        },
+                                        {
+                                            "x": df["Date"],
+                                            "y": df["ConfirmedCovidCases"],
+                                            "type": "bar",
+                                            "name": "Confirmed Cases",
+                                        },
+                                    ],
+                                    "layout": {"title": "Total Confirmed Cases"},
+                                },
+                            ),
+                        ]
+                    ),
+                    # New Div for all elements in the new 'row' of the page
+                    html.Div(
+                        [
+                            html.H1(children="Hello Dash 2"),
+                            html.Div(
+                                children="""
+            Dash: A web application framework for Python.
+        """
+                            ),
+                            dcc.Graph(
+                                id="fig2",
+                                figure={
+                                    "data": [
+                                        {
+                                            "x": df["Date"],
+                                            "y": df["TotalConfirmedCovidCases"],
+                                            "type": "bar",
+                                            "name": "Total Confirmed Cases",
+                                        },
+                                        {
+                                            "x": df["Date"],
+                                            "y": df["ConfirmedCovidCases"],
+                                            "type": "bar",
+                                            "name": "Confirmed Cases",
+                                        },
+                                    ],
+                                    "layout": {"title": "Total Confirmed Cases 2"},
+                                },
+                            ),
+                        ]
+                    ),
+                ]
+            ),
+        ]
+    )
+
+
+# html.Div(
+# [
+# html.Div(
+#     className="Row",
+#     children=[
+#         html.Div(
+#             className="col-lg-12 page-header",
+#             children=[
+#                 html.H3(
+#                     className="text-center",
+#                     children="Dynamic",
+#                 )
+#             ],
+#         )
+#     ],
+# ),
+# html.Div(
+#     className="Row",
+#     children=[
+#         html.Div(className="col-lg-2"),
+#         html.Div(
+#             className="col-lg-8",
+#             children=[
+#                 html.Div(
+#                     className="col-lg-6",
+#                     children=[
+#                         html.Img(
+#                             className="img-responsive",
+#                             src="/static/plotly(8).png",
+#                         ),
+#                         html.Img(
+#                             className="img-responsive",
+#                             src="/static/plotly(9).png",
+#                         ),
+#                     ],
+#                 ),
+#                 html.Div(
+#                     className="col-lg-6",
+#                     children=[
+#                         html.Img(
+#                             className="img-responsive",
+#                             src="/static/plotly(10).png",
+#                         ),
+#                         html.Img(
+#                             className="img-responsive",
+#                             src="/static/plotly(12).png",
+#                         ),
+#                     ],
+#                 ),
+#             ],
+#         ),
+#         html.Div(className="col-lg-2"),
+#     ],
+# ),
+#     ]
+# )
---
+++ flask_dash/apps/fixed.py
@@ -0,0 +1,65 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from flask import url_for
+
+from flask_dash.app import app
+
+
+def layout():
+    return html.Div(
+        [
+            html.Div(
+                className="Row",
+                children=[
+                    html.Div(
+                        className="col-lg-12 page-header",
+                        children=[
+                            html.H3(
+                                className="text-center",
+                                children="Fixed",
+                            )
+                        ],
+                    )
+                ],
+            ),
+            html.Div(
+                className="Row",
+                children=[
+                    html.Div(className="col-lg-2"),
+                    html.Div(
+                        className="col-lg-8",
+                        children=[
+                            html.Div(
+                                className="col-lg-6",
+                                children=[
+                                    html.Img(
+                                        className="img-responsive",
+                                        src="/static/plotly(8).png",
+                                    ),
+                                    html.Img(
+                                        className="img-responsive",
+                                        src="/static/plotly(9).png",
+                                    ),
+                                ],
+                            ),
+                            html.Div(
+                                className="col-lg-6",
+                                children=[
+                                    html.Img(
+                                        className="img-responsive",
+                                        src="/static/plotly(10).png",
+                                    ),
+                                    html.Img(
+                                        className="img-responsive",
+                                        src="/static/plotly(12).png",
+                                    ),
+                                ],
+                            ),
+                        ],
+                    ),
+                    html.Div(className="col-lg-2"),
+                ],
+            ),
+        ]
+    )
