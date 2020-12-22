0c35ba7b289b5ce11e9d0590966659d573588d4f
James Troy <james.m.troy@gmail.com>
2020-12-22 22:41:57 +0000

    refactor to use extra pages

--- flask_dash/app.py
+++ flask_dash/app.py
@@ -1,71 +1,22 @@
-import os
 import json
+import logging
+import os
 
-from flask import Flask
-from flask_debugtoolbar import DebugToolbarExtension
 import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+from flask_caching import Cache
 
-import logging
-
-logging.basicConfig(filename="local.log", level=logging.DEBUG)
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-with open(os.environ.get("CONFIG_FILE", f"{dir_path}/local_config.json"), "r") as f:
-    config = json.load(f)
-
-server = Flask(__name__)
-
-app = dash.Dash(name="Flask Dash App", server=server)
-# set a 'SECRET_KEY' to enable the Flask session cookies
-app.config.update(config)
-# app.debug = True
-# toolbar = DebugToolbarExtension(app)
-
-colors = {"background": "#AAA", "text": "#7FDBFF"}
-
-app.layout = html.Div(
-    style={"backgroundColor": colors["background"]},
-    children=[
-        html.H1(
-            children="This is a H1 ",
-            style={"textAlign": "center", "color": colors["text"]},
-        ),
-        html.Div(
-            children="Dash: Sample Flask App",
-            style={"textAlign": "center", "color": colors["text"]},
-        ),
-        dcc.Graph(
-            id="example-graph-2",
-            figure={
-                "data": [
-                    {"x": [1, 2, 3], "y": [4, 1, 2], "type": "line", "name": "Fridge"},
-                    {"x": [1, 2, 3], "y": [2, 4, 5], "type": "line", "name": "Freezer"},
-                ],
-                "layout": {
-                    "images": [
-                        {
-                            "xref": "paper",
-                            "yref": "paper",
-                            "x": 1,
-                            "y": 1.05,
-                            "sizex": 0.2,
-                            "sizey": 0.2,
-                            "xanchor": "right",
-                            "yanchor": "bottom",
-                        }
-                    ],
-                    "plot_bgcolor": colors["background"],
-                    "paper_bgcolor": colors["background"],
-                    "font": {"color": colors["text"]},
-                },
-            },
-        ),
-    ],
-)
-
-if __name__ == "__main__":
-    app.logger.info("Processing default request")
-    debug = app.config.get("DEBUG", False)
-    app.run_server(debug=debug)
+app = dash.Dash(__name__)
+server = app.server
+cache_enabled = app.config.get("cache", {}).get("enabled", False)
+if cache_enabled:
+    # todo: Add caching for storing daily data
+    CACHE_CONFIG = {
+        "CACHE_TYPE": "redis",
+        "CACHE_REDIS_URL": app.config.get("cache", {}).get("url"),
+    }
+    cache = Cache()
+    cache.init_app(server, config=CACHE_CONFIG)
---
+++ flask_dash/index.py
@@ -0,0 +1,97 @@
+import base64
+import json
+import logging
+import os
+
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table_experiments as dt
+import flask
+import pandas as pd
+from dash.dependencies import Input, Output
+from flask import send_from_directory
+
+from flask_dash.app import app
+from flask_dash.apps import dynamic, fixed, home
+
+logging.basicConfig(filename="local.log", level=logging.DEBUG)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+with open(os.environ.get("CONFIG_FILE", f"{dir_path}/local_config.json"), "r") as f:
+    config = json.load(f)
+
+server = flask.Flask(__name__)
+external_stylesheets = []
+external_stylesheets.append(dbc.themes.BOOTSTRAP)
+external_stylesheets.append(dbc.themes.DARKLY)
+dash_settings = config["dash_settings"]
+dash_settings["server"] = server
+dash_settings["external_stylesheets"] = external_stylesheets
+
+app = dash.Dash(__name__, **dash_settings)
+# set a 'SECRET_KEY' to enable the Flask session cookies
+app.config.update(config)
+
+# toolbar = DebugToolbarExtension(app)
+df = pd.read_csv(
+    "https://opendata-geohive.hub.arcgis.com/datasets/d8eb52d56273413b84b0187a4e9117be_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D"
+)
+
+colors = {"background": "#AAA", "text": "#7FDBFF"}
+
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Home", href="/home")),
+        dbc.NavItem(dbc.NavLink("Fixed", href="/fixed")),
+        dbc.NavItem(dbc.NavLink("Dynamic", href="/dynamic")),
+    ],
+    brand=app.title,
+    brand_href="/",
+    sticky="top",
+)
+
+url_bar_navbar_content = html.Div(
+    [
+        dcc.Location(id="url", refresh=False),
+        html.Div(children=[navbar, html.Div(id="page-content")]),
+    ]
+)
+
+
+def serve_layout():
+    if flask.has_request_context():
+        return url_bar_navbar_content
+    return html.Div(
+        [
+            url_bar_navbar_content,
+            home.layout(),
+        ]
+    )
+
+
+app.layout = serve_layout
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def display_page(pathname):
+
+    if pathname in ["/", "/home", "/index"]:
+        return home.layout()
+    elif pathname == "/fixed":
+        return fixed.layout()
+
+    elif pathname == "/dynamic":
+        return dynamic.layout()
+
+    else:
+        return "Error 404"
+
+
+if __name__ == "__main__":
+    app.logger.info("Processing default request")
+    debug = app.config.get("DEBUG", False)
+    app.run_server(debug=debug, threaded=True)
+    # app.run_server(host='0.0.0.0', debug=True, threaded=True)
